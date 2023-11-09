9adbccc132a1cac225458cfcc058186d42001930
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-11-09 02:09:57 +0200

    fix-bug 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,27 +1,27 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import trottle from 'lodash.throttle';
 
-const iframeVideoElem = document.querySelector('.ifreme');
+const iframe = document.querySelector('#vimeo-player');
 
-const player = new Player(iframeVideoElem);
-// Подія timeupdate спрацьовує кожного разу, коли змінюється положення відтворення відео.
-player.on('timeupdate', throttle(onTimeUpdate, 1000));
-//  зберігає поточне положення відтворення  у localStorage
-function onTimeUpdate(data) {
+const player = new Player(iframe, {
+  width: 640,
+});
+
+function onPlay(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 }
-//  отримує поточне положення відтворення  у localStorage
-const savedDataSeconds = localStorage.getItem('videoplayer-current-time');
 
-//  встановлення положення відтворення відео на збережене значення, отримане з локального сховища.
+player.on('timeupdate', trottle(onPlay), 1000);
+
+const timeFromLocalStorage = localStorage.getItem('videoplayer-current-time');
+
 player
-  .setCurrentTime(savedDataSeconds)
+  .setCurrentTime(timeFromLocalStorage)
   .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
         break;
-
       default:
         break;
     }
